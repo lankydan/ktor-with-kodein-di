13bd9a9e826f08f0d1d23745d4846ebf7add79cb
LankyDan <danknewton@hotmail.com>
2022-08-15 21:41:45 +0100

    Some more WebSocket code
    Includes commented out code so I can look at it in the future
    if needed.

--- src/main/kotlin/dev/lankydan/ktor/web/People.kt
+++ src/main/kotlin/dev/lankydan/ktor/web/People.kt
@@ -2,16 +2,29 @@ package dev.lankydan.ktor.web
 
 import dev.lankydan.ktor.data.Person
 import dev.lankydan.ktor.repository.PersonRepository
-import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import io.ktor.server.websocket.*
-import io.ktor.websocket.*
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.call
+import io.ktor.server.request.receive
+import io.ktor.server.response.respond
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.Routing
+import io.ktor.server.routing.delete
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.put
+import io.ktor.server.routing.route
+import io.ktor.server.websocket.webSocket
+import io.ktor.websocket.CloseReason
+import io.ktor.websocket.Frame
+import io.ktor.websocket.close
+import io.ktor.websocket.readText
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.ClosedReceiveChannelException
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
 import java.util.*
 
 private enum class PeopleFilter {
@@ -57,30 +70,121 @@ fun Routing.people(personRepository: PersonRepository) {
     }
     webSocket {
       var filter = PeopleFilter.ALL
-      incoming.receiveAsFlow().onEach { frame ->
-        (frame as? Frame.Text)?.let { text ->
-          filter = when(text.readText()) {
-            "saves" -> PeopleFilter.SAVES
-            "deletes" -> PeopleFilter.DELETES
-            else -> PeopleFilter.ALL
+//      incoming.receiveAsFlow().onEach { frame ->
+//        (frame as? Frame.Text)?.let { text ->
+//          filter = when (text.readText()) {
+//            "saves" -> PeopleFilter.SAVES
+//            "deletes" -> {
+//              PeopleFilter.DELETES
+//            }
+//            else -> PeopleFilter.ALL
+//          }
+//        }
+//      }.launchIn(this)
+
+//      launch {
+//        for (frame in incoming) {
+//          (frame as? Frame.Text)?.let { text ->
+//            filter = when (text.readText()) {
+//              "saves" -> PeopleFilter.SAVES
+//              "deletes" -> PeopleFilter.DELETES
+//              "stop" -> return@webSocket // Terminates the WebSocket
+//              else -> PeopleFilter.ALL
+//            }
+//          }
+//        }
+//      }
+
+      // throwing an exception does not work
+//      launch {
+//        for (frame in incoming) {
+//          (frame as? Frame.Text)?.let { text ->
+//            filter = when (text.readText()) {
+//              "saves" -> PeopleFilter.SAVES
+//              "deletes" -> PeopleFilter.DELETES
+//              "stop" -> throw RuntimeException("Finish") // Terminates the WebSocket
+//              else -> PeopleFilter.ALL
+//            }
+//          }
+//        }
+//      }
+
+      launch {
+        for (frame in incoming) {
+          (frame as? Frame.Text)?.let { text ->
+            filter = when (text.readText()) {
+              "saves" -> PeopleFilter.SAVES
+              "deletes" -> PeopleFilter.DELETES
+              "stop" -> {
+                close() // Terminates the WebSocket, but does not return from the [webSocket] handler, so the handler is still live and receives updates
+//                throw ClosedReceiveChannelException("closeReason.await()?.message")
+//                cancel()
+                // ONLY NEED THE CLOSE, HOWEVER IT DOES LET THE NEXT UPDATE STILL EXECUTE
+                return@let
+//                this.can
+              }
+              else -> PeopleFilter.ALL
+            }
           }
         }
-      }.launchIn(this)
+      }
 
       personRepository.updates.collect { (id, person) ->
         when (person) {
           null -> {
             if (filter == PeopleFilter.ALL || filter == PeopleFilter.DELETES) {
+              println("delete")
               outgoing.send(Frame.Text("Deleted person [$id]"))
             }
           }
           else -> {
             if (filter == PeopleFilter.ALL || filter == PeopleFilter.SAVES) {
+              println("save")
               outgoing.send(Frame.Text("Saved person [$id] $person"))
             }
           }
         }
       }
+
+        val incomingJob = incoming.receiveAsFlow().onEach { frame ->
+          (frame as? Frame.Text)?.let { text ->
+            filter = when (text.readText()) {
+              "saves" -> PeopleFilter.SAVES
+              "deletes" -> PeopleFilter.DELETES
+              "stop" -> {
+                close(CloseReason(CloseReason.Codes.NORMAL, "close this socket")) // Terminates the WebSocket, but does not return from the [webSocket] handler, so the handler is still live and receives updates
+                return@let
+              }
+              else -> PeopleFilter.ALL
+            }
+          }
+        }.launchIn(this)
+
+      // after closing the flow will still execute once when an update comes in unless it is cancelled
+      val outgoingJob = personRepository.updates.onEach { (id, person) ->
+        when (person) {
+          null -> {
+            if (filter == PeopleFilter.ALL || filter == PeopleFilter.DELETES) {
+              println("delete")
+              outgoing.send(Frame.Text("Deleted person [$id]"))
+            }
+          }
+          else -> {
+            if (filter == PeopleFilter.ALL || filter == PeopleFilter.SAVES) {
+              println("save")
+              outgoing.send(Frame.Text("Saved person [$id] $person"))
+            }
+          }
+        }
+      }.launchIn(this)
+
+      val reason = closeReason.await()
+
+      incomingJob.cancel()
+      outgoingJob.cancel()
+      joinAll(incomingJob, outgoingJob)
+
+      println("Closed websocket: ${reason?.message}")
     }
   }
 }
